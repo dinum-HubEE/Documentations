5409a0dd1579043f6314109836bd941ec29fa70b
Adrien Carpentier <me@adriencarpentier.com>
2025-08-18 19:49:13 +0200

    refactor: rename loop var

--- SolutionSI/main.py
+++ SolutionSI/main.py
@@ -14,17 +14,17 @@ def process_hubee_telefolders(
         print("Il n'y a pas de notification")
         return
 
-    for notification in notifications:
-        print("Traitement de la notification:", notification["id"])
-        # print(notification)
+    for notif in notifications:
+        print("Traitement de la notification:", notif["id"])
+        # print(notif)
         # traitement d'une notification
-        if notification["eventId"] is None:
-            case = hubee_api.get_case(token, notification["caseId"])
+        if notif["eventId"] is None:
+            case = hubee_api.get_case(token, notif["caseId"])
 
             for PJ in case["attachments"]:
                 hubee_api.download_case_attachment(
                     token,
-                    notification["caseId"],
+                    notif["caseId"],
                     PJ["id"],
                     PJ["fileName"],
                     case["externalId"],
@@ -33,24 +33,24 @@ def process_hubee_telefolders(
 
             hubee_api.create_status_event(
                 token,
-                notification["caseId"],
+                notif["caseId"],
                 hubee_api.config["status_minimal"],
             )
             hubee_api.create_status_event(
                 token,
-                notification["caseId"],
+                notif["caseId"],
                 hubee_api.config["status_maximal"],
             )
-            hubee_api.delete_notification(token, notification["id"])
+            hubee_api.delete_notification(token, notif["id"])
         else:
-            if notification["eventStatus"] == "RECEIVED":
-                hubee_api.delete_notification(token, notification["id"])
+            if notif["eventStatus"] == "RECEIVED":
+                hubee_api.delete_notification(token, notif["id"])
 
             else:
                 event = hubee_api.get_event(
                     token,
-                    notification["caseId"],
-                    notification["eventId"],
+                    notif["caseId"],
+                    notif["eventId"],
                 )
 
                 match event["actionType"]:
@@ -68,14 +68,14 @@ def process_hubee_telefolders(
                         print("message  [", event["message"], "]")
                     case "ATTACH_DEPOSIT":
                         # ceci est un event ATTACH_DEPOSIT
-                        case = hubee_api.get_case(token, notification["caseId"])
+                        case = hubee_api.get_case(token, notif["caseId"])
 
                         for PJ in event["attachments"]:
                             # téléchargement des Pjs de l'event
                             hubee_api.download_event_attachment(
                                 token,
-                                notification["caseId"],
-                                notification["eventId"],
+                                notif["caseId"],
+                                notif["eventId"],
                                 PJ["id"],
                                 PJ["fileName"],
                                 case["externalId"],
@@ -85,12 +85,12 @@ def process_hubee_telefolders(
                         # changement des status du case et création d'events
                         hubee_api.create_status_event(
                             token,
-                            notification["caseId"],
+                            notif["caseId"],
                             hubee_api.config["status_minimal"],
                         )
                         hubee_api.create_status_event(
                             token,
-                            notification["caseId"],
+                            notif["caseId"],
                             hubee_api.config["status_maximal"],
                         )
                     case _:
@@ -98,8 +98,8 @@ def process_hubee_telefolders(
 
                 hubee_api.update_event_status(
                     token,
-                    notification["caseId"],
-                    notification["eventId"],
+                    notif["caseId"],
+                    notif["eventId"],
                     "RECEIVED",
                 )
 
