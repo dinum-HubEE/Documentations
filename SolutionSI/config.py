a03b68ec0ccd543a7e7da45993dd1ba3088ff40a
Adrien Carpentier <me@adriencarpentier.com>
2025-08-18 18:26:38 +0200

    style: lint config.py

--- SolutionSI/config.py
+++ SolutionSI/config.py
@@ -1,28 +1,27 @@
 config = {
-    'environnement' :{
-        'token':'',
-        'api':''
+    "environnement": {"token": "", "api": ""},
+    "notificationMax": 25,
+    "acteurType": "SI",
+    "statusMinimal": "IN_PROGRESS",
+    "statusMaximal": "DONE",
+    "NombreRetry": 5,
+    "header": {
+        "editorName": "SI_XYZ",
+        "applicationName": "script_HUBEE_DINUM",
+        "softwareVersion": "1.0.1",
     },
-    'notificationMax': 25,
-    'acteurType':'SI',
-    'statusMinimal':'IN_PROGRESS',
-    'statusMaximal':'DONE',
-    'NombreRetry': 5,
-    'header':{
-        'editorName':'SI_XYZ',
-        'applicationName':'script_HUBEE_DINUM',
-        'softwareVersion':'1.0.1'
-    },
-    'demarches' :[{
-        'demarcheNom':'CERTDC',
-        'clientId':'',
-        'clientSecret':'',
-        'dossierDeTelechargement':'data/file/download/CERTDC/',
-    },
-    {
-        'demarcheNom':'EtatCivil',
-        'clientId':'',
-        'clientSecret':'',
-        'dossierDeTelechargement':'data/file/download/EtatCivil/',
-    }],
+    "demarches": [
+        {
+            "demarcheNom": "CERTDC",
+            "clientId": "",
+            "clientSecret": "",
+            "dossierDeTelechargement": "data/file/download/CERTDC/",
+        },
+        {
+            "demarcheNom": "EtatCivil",
+            "clientId": "",
+            "clientSecret": "",
+            "dossierDeTelechargement": "data/file/download/EtatCivil/",
+        },
+    ],
 }
